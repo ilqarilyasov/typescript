91f20082a9b5e036a4ac88660178e56f2ca30721
Ilgar Ilyasov <iilyasov@awarex.com>
2020-05-07 09:08:10 -0400

    Create first main file

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var message = 'Welcome back';
+console.log(message);
+var x = 10; // can be without init value
+var y = 20; // always init value
+var sum;
+var title = 'Learn Typescript';
---
+++ main.ts
@@ -0,0 +1,9 @@
+export {}
+let message = 'Welcome back';
+console.log(message);
+
+let x = 10; // can be without init value
+const y = 20; // always init value
+
+let sum;
+const title = 'Learn Typescript';
